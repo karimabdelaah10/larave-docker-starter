3d9b0dea143dfc31f2f157300c73679859589a13
karimabdelaah <karim.abdullah@our-edu.net>
2023-07-27 13:15:15 +0300

    fix ignored folders from storage

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 cd /var/www
-find /var/www -exec chown -R nobody:www-data {} \; && find /var/www -type f -exec chmod 664 {} \; && find /var/www -type d -exec chmod 775 {} \;
+
+mkdir -p storage/framework/cache/data
 php artisan route:trans:cache
 php artisan migrate --force
 php artisan cache:clear
@@ -9,10 +10,9 @@ php artisan cache:clear
 php-fpm
 
 chown -R www-data:www-data /var/www
-chmod -R 755 /var/www/storage
+chmod -R 775 /var/www/storage
+
 composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
-php artisan key:generate
-php artisan cache:clear
-php artisan config:cache
 
 /usr/bin/supervisord -c /etc/supervisord.conf
+
